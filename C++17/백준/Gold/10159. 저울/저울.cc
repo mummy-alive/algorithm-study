d28c9891ded69b5bf08cf18d6fdca503b08b5e93
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-08-05 11:56:46 +0900

    [Gold IV] Title: 저울, Time: 0 ms, Memory: 1132 KB -BaekjoonHub

--- C++17/백준/Gold/10159. 저울/저울.cc
+++ C++17/백준/Gold/10159. 저울/저울.cc
@@ -1,43 +1,24 @@
-#include<iostream>
-#include<cstring>
-#include<vector>
-using namespace std;
-int n, m;
-bool c1[105], c2[105];
-vector<int> vt1[105], vt2[105];
-void fnd1(int n) {
-	if (c1[n]) return;
-	c1[n] = 1;
-	for (auto x : vt1[n]) {
-		fnd1(x);
-	}
-}
-
-void fnd2(int n) {
-	if (c2[n]) return;
-	c2[n] = 1;
-	for (auto x : vt2[n]) {
-		fnd2(x);
-	}
-}
+#include <cstdio>
+int n, m, a, b, s, ans;
+bool d[105][105], e[105][105];
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		int u, v;
-		cin >> u >> v;
-		vt1[u].push_back(v);
-		vt2[v].push_back(u);
+	int i, j, k;
+	scanf("%d%d", &n, &m);
+	while(m--) {
+		scanf("%d%d", &a, &b);
+		d[a][b] = 1;
+		e[b][a] = 1;
 	}
-	for (int i = 1; i <= n; i++) {
-		memset(c1, 0, sizeof(c1));
-		memset(c2, 0, sizeof(c2));
-		fnd1(i);
-		fnd2(i);
-		int cnt = 0;
-		for (int j = 1; j <= n; j++)
-			if (c1[j] || c2[j])
-				cnt++;
-		cout << n - cnt << '\n';
+	for(k=1; k<=n; k++) for(i=1; i<=n; i++) {
+		if(d[i][k])
+			for(j=1; j<=n; j++) if(d[k][j]) d[i][j] = 1;
+		if(e[i][k])
+			for(j=1; j<=n; j++) if(e[k][j]) e[i][j] = 1;
+	}
+	for(i=1; i<=n; i++) {
+		s = 0;
+		for(j=1; j<=n; j++) s += d[i][j] + e[i][j];
+    	printf("%d\n", n-s-1);
 	}
 	return 0;
 }

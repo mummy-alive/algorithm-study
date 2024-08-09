e2a2e8d32a7109d053013f149eed0d00cd9c743c
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-08-09 11:18:11 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- C++17/백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ C++17/백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -2,16 +2,14 @@
 #define MD 1000000007
 using namespace std;
 typedef long long LL;
-LL n, dp[5005];
+LL n, dp[5005], plur = 1;
 int main() {
 	cin >> n;
 	dp[0] = 1;
 	dp[2] = 3;
 	for (LL i = 4; i <= n; i+=2) {
-		dp[i] = dp[i - 2] * 3;
-		for (LL j = i - 4; j >= 0; j-=2) {
-			dp[i] += dp[j] * 2;
-		}
+		dp[i] = dp[i - 2] * 3 + plur * 2;
+		plur += dp[i - 2];
 	}
 	cout << dp[n];
 	return 0;

a8c9286f9def0cf7afd69c64c77b97fe55b23236
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-08-14 11:10:17 +0900

    [Silver II] Title: 창고 다각형, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- C++17/백준/Silver/2304. 창고 다각형/창고 다각형.cc
+++ C++17/백준/Silver/2304. 창고 다각형/창고 다각형.cc
@@ -1,8 +1,6 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
-int n;
+int n, sum;
 int arr[1005], ln, lt[1005], rt[1005];
 int main() {
 	cin >> n;
@@ -10,16 +8,13 @@ int main() {
 		int l, h;
 		cin >> l >> h;
 		arr[l] = h;
-		ln = max(ln, l);
 	}
-	for (int i = 1; i <= ln; i++) 
+	for (int i = 1; i <= 1000; i++) 
 		lt[i] = max(lt[i - 1], arr[i]);
-	for (int i = ln; i >= 0; i--) 
+	for (int i = 1000; i >= 0; i--) {
 		rt[i] = max(rt[i + 1], arr[i]);
-
-	int sum = 0;
-	for (int i = 1; i <= ln; i++)
 		sum += min(lt[i], rt[i]);
+	}
 	cout << sum;
 	return 0;
 }

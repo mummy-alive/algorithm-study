8412ce8b671750292f1c9f405baa72c13bac328b
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-08-16 11:41:37 +0900

    [Silver I] Title: 경비원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/2564. 경비원/경비원.cc
+++ C++17/백준/Silver/2564. 경비원/경비원.cc
@@ -1,51 +1,23 @@
 #include<iostream>
 using namespace std;
-int x, y, n, nm[105], nm_c;
-pair<int, int> pr[105], cr;
-
-int f() {
-	int cnt = 0;
-	int dx[] = { 0, 0, x, x };
-	int dy[] = { 0, y, 0, y };
-	for (int i = 0; i < n; i++) {
-		if (nm_c == nm[i]) {
-			cnt += max(abs(pr[i].first - cr.first), abs(pr[i].second - cr.second));
-			continue;
-		}
-		int par = 1e9;
-		for (int j = 0; j < 4; j++) {
-			par = min(par, abs(pr[i].first - dx[j]) + abs(pr[i].second - dy[j]) + abs(cr.first - dx[j]) + abs(cr.second - dy[j]));
-		}
-		cnt += par;
-	}
-	return cnt;
-}
-
-pair<int, int> fixer(int a, int b) {
-	pair<int, int> pk;
-	if (a == 1)
-		return { b, 0 };
-	if (a == 2) 
-		return { b, y };
-	if (a == 3)
-		return { 0, b };
-	if (a == 4)
-		return { x, b };
-}
-
+int x, y, k, a[105], b[105], arr[105], ans;
 int main() {
-	cin >> x >> y;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
+	cin >> x >> y >> k;
+	for (int i = 0; i <= k; i++) {
 		int a, b;
 		cin >> a >> b;
-		nm[i] = a;
-		pr[i] = fixer(a, b);
+		if (a == 1) arr[i] = b;
+		else if (a == 4)
+			arr[i] = b + x;
+		else if (a == 2)
+			arr[i] = (x - b) + x + y;
+		else if (a == 3)
+			arr[i] = (y - b) + x + x + y;
 	}
-	int p, q;
-	cin >> p >> q;
-	nm_c = p;
-	cr = fixer(p, q);
-	cout << f();
+	for (int i = 0; i < k; i++) {
+		int par = abs(arr[k] - arr[i]);
+		ans += min(par, 2 * (x + y) - par);
+	}
+	cout << ans;
 	return 0;
 }

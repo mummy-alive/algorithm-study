f23b71309a0cb4042a76a8db26462dfcad44f875
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-08-21 10:58:43 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/11057. 오르막 수/오르막 수.cc
+++ C++17/백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -2,16 +2,12 @@
 #define MD 10007
 using namespace std;
 int n, ans;
-int dp[15][1005];
+int dp[15] = { 1 };
 int main() {
 	cin >> n;
-	for (int i = 0; i < 10; i++)
-		dp[i][1] = 1;
-	for (int j = 2; j <= n; j++)
-		for (int i = 0; i < 10; i++)
-			dp[i][j] = (dp[max(0, i - 1)][j] + dp[i][j - 1]) % MD;
-	for (int i = 0; i < 10; i++)
-		ans = (ans + dp[i][n]) % MD;
-	cout << ans;
+	for (int j = 1; j <= n+1; j++)
+		for (int i = 1; i < 10; i++)
+			dp[i] = (dp[i] + dp[i - 1]) % MD;
+	cout << dp[9];
 	return 0;
 }

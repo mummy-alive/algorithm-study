1e7d251457bba2e6ff1f4fedf4365546422e12aa
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-18 14:42:37 +0900

    [level 2] Title: N-Queen, Time: 147.14 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/2/12952. N－Queen/N－Queen.cpp
+++ C++/프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -1,35 +1,46 @@
 #include <string>
 #include <vector>
-
+#include <cstring>
 using namespace std;
-int ans = 0;
-int sz;
-int col[15];
-bool arr[15][15] = {0};
+bool idx[15][15] = {0};
 
-bool chk(int k){
-    for(int i=0; i<k; i++){
-        if(col[k] == col[i] || k-i == abs(col[k] - col[i]))
-            return false;
-    }    
+bool chk(int sz, int r, int c){
+    for(int i=r-1; i>0; i--){
+        if(idx[i][c])
+            return 0;
+    }
+    for(int i=1; i<r; i++){
+        if(c+i > sz) break;
+        if(idx[r-i][c+i])
+            return 0;
+    }
+    for(int i=1; i<r; i++){
+        if(c-i <= 0) break;
+        if(idx[r-i][c-i])
+            return 0;
+    }
     return true;
 }
 
-void f(int k){
-    if(k == sz) {
-        ans++;
-        return;
-    }
-    for(int i=0; i<sz; i++) {
-        col[k] = i;
-        if(chk(k)) {
-            f(k+1);
+int f(int sz, int r, int c) {
+    if(r >= sz) return 1;
+    int sum = 0;
+    for(int j=1; j<=sz; j++){
+        if(chk(sz, r+1, j)) {
+            idx[r+1][j] = 1;
+            sum += f(sz, r + 1, j);
+            idx[r+1][j] = 0;
         }
     }
+    return sum;
 }
 
 int solution(int n) {
-    sz = n;
-    f(0);
-    return ans;
+    int answer = 0;
+    for(int j=1; j<=n; j++){
+        idx[1][j] = 1;
+        answer += f(n, 1, j);
+        idx[1][j] = 0;
+    }
+    return answer;
 }

6c425d64149e91e409912e9b0fae2eb462690668
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-21 16:16:49 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ C++17/백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,36 +1,36 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
-int ch[30];
-char ans[55];
-string st;
+string st, ans = "";
+int cnt[130] = { 0 };
 int main() {
 	cin >> st;
 	int n = st.size();
-	for (int i = 0; i < n; i++) {
-		ch[st[i] - 'A']++;
-	}
-	int p = 0;
-	for (int i = 0; i < 26; i++) {
-		if (ch[i] % 2) {
-			if ((n % 2) && !ans[n / 2]) {
-				ch[i]--;
-				ans[n / 2] = char(i + 'A');
-			}
-			else {
+	for (int i = 0; i < n; i++) 
+		cnt[st[i]]++;
+	bool flg = 0;
+	for (int i = 'Z'; i >= 'A'; i--) {
+		if (cnt[i] % 2) {
+			if (!(n % 2)) {
 				cout << "I'm Sorry Hansoo";
 				return 0;
 			}
+			else {
+				if (flg) {
+					cout << "I'm Sorry Hansoo";
+					return 0;
+				}
+				flg = 1;
+				ans = char(i);
+				cnt[i]--;
+			}
 		}
 	}
-	for (int i = 0; i < 26; i++) {
-		for (int j = 0; j < ch[i]; j += 2) {
-			ans[p] = (char)(i + 'A');
-			ans[n - 1 - p] = (char(i + 'A'));
-			p++;
+	for (int i = 'Z'; i >= 'A'; i--) {
+		while (cnt[i]) {
+			ans = char(i) + ans + char(i);
+			cnt[i] -= 2;
 		}
 	}
-	for (int i = 0; i < n; i++)
-		cout << ans[i];
+	cout << ans;
 	return 0;
 }

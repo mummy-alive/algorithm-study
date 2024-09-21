97e1b778da1ea8fef2a4189a421019415d6af0e2
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-21 16:40:48 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 10.39 ms, Memory: 4.45 MB -BaekjoonHub

--- C++/프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ C++/프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,47 +1,45 @@
 #include <string>
-#include <iostream>
 #include <vector>
-#include <map>
 #include <sstream>
+#include <map>
 using namespace std;
 
 int solution(vector<string> friends, vector<string> gifts) {
     int answer = 0;
     map<string, int> mp;
-    int arr[55][55] = {0}, gft[55] = {0}, res[55] = {0};
-    for(int i=0; i<friends.size(); i++)
-        mp[friends[i]] = i;
-	for(auto x: gifts) {
-		istringstream iss(x);
+    int arr[55][55] = {0}, gft[55] = {0}, ans[55] = {0};
+    int n = 0;
+    for(auto x: friends){
+        mp[x] = n;
+        n++;
+    }
+    for(auto x: gifts){
+        istringstream iss(x);
         string a, b;
-		iss >> a >> b;
+        iss >> a >> b;
         arr[mp[a]][mp[b]]++;
         gft[mp[a]]++;
         gft[mp[b]]--;
     }
-    for(int p=0; p<friends.size(); p++){
-        for(int i=p+1; i<friends.size(); i++){
-            if(arr[p][i] == arr[i][p]){
-                if(gft[p] > gft[i])
-                    res[p]++;
-                else if(gft[i] > gft[p])
-                    res[i]++;
-                else continue;
-            }  
-            else if(arr[i][p] > arr[p][i]) {
-                res[i]++;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i][j] - arr[j][i] != 0){
+                if(arr[i][j] > arr[j][i])
+                    ans[i]++;
+                else ans[j]++;
             }
-            else {
-                res[p]++;
+            else{
+                if(gft[i]!=gft[j]){
+                    if(gft[i] > gft[j])
+                        ans[i]++;
+                    else ans[j]++;
+                }
             }
         }
     }
-    int mx = 0;
-    for(int i=0; i<friends.size(); i++){
-        if(res[i] > res[mx])
-            mx = i;
-    }
-    answer = res[mx];
-    return answer;
+    for(int i=0; i<n; i++)
+        answer = max(answer, ans[i]);
     
+    
+    return answer;
 }

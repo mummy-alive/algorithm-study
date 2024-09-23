24b51cbed601e132d50e825a912da8698ae48703
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-23 21:54:20 +0900

    [Silver III] Title: 햄버거 분배, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Silver/19941. 햄버거 분배/햄버거 분배.cc
+++ C++17/백준/Silver/19941. 햄버거 분배/햄버거 분배.cc
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
-int n, k, ans;
-bool idx[20005];
-string st;
 int main() {
+	int n, k;
+	int ans = 0;
+	string st;
+	bool ham[20005] = { 0 };
 	cin >> n >> k;
 	cin >> st;
 	for (int i = 0; i < n; i++) {
-		if (st[i] == 'P') {
-			for (int j = max(0, i - k); j <= min(n - 1, i + k); j++) {
-				if ((st[j] == 'H') && !idx[j]) {
-					ans++;
-					idx[j] = 1;
-					break;
-				}
+		if (st[i] == 'H') continue;
+		for (int j = max(0, i - k); j <= min(n - 1, i + k); j++) {
+			if ((st[j] == 'H') && !ham[j]) {
+				ham[j] = 1;
+				ans++;
+				break;
 			}
 		}
 	}

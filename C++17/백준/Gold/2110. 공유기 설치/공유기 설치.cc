e03e432c6497f92011512f83196273470decaae9
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-23 21:15:12 +0900

    [Gold IV] Title: 공유기 설치, Time: 28 ms, Memory: 2800 KB -BaekjoonHub

--- C++17/백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ C++17/백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int n, c, arr[200005], ans;
+int n, c, arr[200005], mx;
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> c;
@@ -10,22 +10,21 @@ int main() {
 	sort(arr, arr + n);
 	int lt = 1, rt = arr[n - 1] - arr[0];
 	while (lt <= rt) {
-		int cnt = 1;
 		int md = (lt + rt) / 2;
+		int cnt = 1;
 		int st = arr[0];
 		for (int i = 1; i < n; i++) {
-			if (arr[i] - st >= md) {
+			if ((arr[i] - st) >= md) {
 				cnt++;
 				st = arr[i];
 			}
 		}
 		if (cnt >= c) {
-			ans = max(ans, md);
+			mx = max(mx, md);
 			lt = md + 1;
 		}
-		else 
-			rt = md - 1;
+		else rt = md - 1;
 	}
-	cout << ans;
+	cout << mx;
 	return 0;
 }

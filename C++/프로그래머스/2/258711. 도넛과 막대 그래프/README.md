bac19df6ba6e264098a875c6ac413920e1fb6619
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-24 20:48:42 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 273.42 ms, Memory: 125 MB -BaekjoonHub

--- C++/프로그래머스/2/258711. 도넛과 막대 그래프/README.md
+++ C++/프로그래머스/2/258711. 도넛과 막대 그래프/README.md
@@ -1,10 +1,10 @@
 # [level 2] 도넛과 막대 그래프 - 258711 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711?language=cpp) 
 
 ### 성능 요약
 
-메모리: 156 MB, 시간: 215.52 ms
+메모리: 125 MB, 시간: 273.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 02일 10:57:01
+2024년 09월 24일 20:48:56
 
 ### 문제 설명
 
--- C++/프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ C++/프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -1,66 +1,49 @@
 #include <string>
-#include <iostream>
+#include <cstring>
 #include <queue>
 #include <vector>
 #define SZ 1000005
 using namespace std;
-int donut, bar, eight;
-vector<int> vt[SZ];
-bool idx[SZ];
-
-void bfs(int v){
-    queue<int> qu;
-    qu.push(v);
-    while (!qu.empty()) {
-        int k = qu.front();
-//        cout<<k<<"pops\n";
-        qu.pop();
-        if(vt[k].size() == 2){
-            eight++;
-            return;
-        }
-        if (idx[k]) {
-            donut++;
-            return;
-        }
-        idx[k] = 1;
-        for(auto x: vt[k])
-            qu.push(x);
-    }
-//    cout<<bar<<'\n';
-    bar++;
-}
-
-
-void f(int v) {
-    bfs(v);
-}
-
 vector<int> solution(vector<vector<int>> edges) {
-    vector<int> answer;
-
-    int ind[SZ]={0}, oud[SZ] ={0};
-    int n = edges.size();
-    for(int i=0; i<n; i++){
-        int u = edges[i][0], v = edges[i][1];
-        vt[u].push_back(v);
-        ind[v]++;
-        oud[u]++;
+    vector<int> answer, vt[SZ];
+    bool idx[SZ] = {0};
+    int ind[SZ] = {0}, oud[SZ] = {0}, st = 0;
+    memset(idx, 0, sizeof(idx));
+    for(auto x: edges){
+        vt[x[0]].push_back(x[1]);
+        ind[x[1]]++; oud[x[0]]++;
     }
-    int start=0;
     for(int i=1; i<SZ; i++){
-        if((ind[i] == 0) && (oud[i] > 1)){
-            start = i;
+        if(ind[i]==0 && (oud[i] >1)){
+            st = i;
             break;
         }
     }
-    answer.push_back(start);
-    for (auto x: vt[start]){
-        f(x);
-//        cout<<x<<" starts\n";
+    answer.push_back(st);
+    for(int i=0; i<3; i++) answer.push_back(0);
+
+    for(auto x: vt[st]) {
+        queue<int> qu;
+        qu.push(x);
+        while(!qu.empty()){
+            int p = qu.front();
+            qu.pop();
+            if(vt[p].size() < 1){
+                answer[2]++;
+                break;
+            }
+            if(vt[p].size() > 1) {
+                answer[3]++;
+                break;
+            }
+            if(idx[p]) {
+                answer[1]++;
+                break;
+            }
+            idx[p]=1;
+            for(auto y: vt[p])
+                qu.push(y);
+        }
     }
-    answer.push_back(donut);
-    answer.push_back(bar);
-    answer.push_back(eight);
     return answer;
 }

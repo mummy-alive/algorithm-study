4c97e08829d0a50e8dee262f04dd278f1ec9511a
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-25 10:49:04 +0900

    [level 2] Title: 문자열 압축, Time: 0.36 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ C++/프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -1,38 +1,31 @@
 #include <string>
 #include <vector>
-#include<iostream>
-#include<cmath>
+#include <cmath>
+
 using namespace std;
 
 int solution(string s) {
-   // s = "abababababababababababababababababababab";
-    int n = s.size();
-    int answer = n ;
-    bool flg = 0;
-    for(int i=1; i<=n/2; i++) {
-        int cnt = 0;
+    int answer = 0;
+    int n= s.size();
+    answer = n;
+    for(int i=1; i<=n/2; i++){
+        int cnt=0, tmp =1;
         string prev = "";
-        int tmp = 1;
-        for(int j=0; j<n; j+=i) {
+        for(int j=0; j<n; j+=i){
             string par = s.substr(j, i);
             if(par == prev){
                 tmp++;
                 continue;
             }
             if(tmp > 1){
-                //cout<<cnt<<' '<<(int)log10(tmp) + 1<<'\n';
-                cnt += (int)log10(tmp) + 1;
+                cnt += (int)log10(tmp) +1 ; // 길이 세는 법
             }
-            tmp = 1;
+            tmp=1;
             cnt += min(i, n-j);
             prev = par;
-            flg = 0;
         }
-        if(tmp > 1){
-            //cout<<cnt<<' '<<(int)log10(tmp) + 1<<'\n';
-            cnt += (int)log10(tmp) + 1;
-        }
-        //cout<<i<<": "<<cnt<<'\n';
+        if(tmp > 1)
+            cnt+=(int)log10(tmp)+1;
         answer = min(answer, cnt);
     }
     return answer;

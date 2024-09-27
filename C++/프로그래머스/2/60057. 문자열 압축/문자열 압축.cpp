136067bc3bb0bcf09163343d2aa569b7eb1c9e0f
mummy <113423544+mummy-alive@users.noreply.github.com>
2024-09-28 00:38:56 +0900

    [level 2] Title: 문자열 압축, Time: 0.49 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ C++/프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -5,27 +5,25 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = 0;
-    int n= s.size();
-    answer = n;
-    for(int i=1; i<=n/2; i++){
-        int cnt=0, tmp =1;
-        string prev = "";
-        for(int j=0; j<n; j+=i){
+    int answer = s.size();
+    int n = s.size();
+    for(int i=1; i<=n/2;i++) {
+        int cnt = 0;
+        int tmp=1;
+        string prev= "";
+        for(int j=0; j <= n; j+=i) {
             string par = s.substr(j, i);
             if(par == prev){
                 tmp++;
                 continue;
             }
             if(tmp > 1){
-                cnt += (int)log10(tmp) +1 ; // 길이 세는 법
+                cnt += (int)log10(tmp) + 1;
             }
-            tmp=1;
+            tmp = 1;
             cnt += min(i, n-j);
             prev = par;
         }
-        if(tmp > 1)
-            cnt+=(int)log10(tmp)+1;
         answer = min(answer, cnt);
     }
     return answer;
